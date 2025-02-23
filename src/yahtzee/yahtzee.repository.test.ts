cf3f218f9718d012d44df272912d48fa5863f8ce
jaranchai <jaranchai.nt@hotmail.com>
2025-02-23 17:18:46 +0700

    add repository and example test

---
+++ src/yahtzee/yahtzee.repository.test.ts
@@ -0,0 +1,51 @@
+import Score, { IScoreInputData, IScoreScheema } from './model/score.model'
+import YahtzeeRepository from './yahtzee.repository'
+
+jest.mock('./model/score.model')
+
+describe('YahtzeeRepository', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should be created', async () => {
+    const score: IScoreInputData = {
+      game: 'game1',
+      player: 'player1',
+      ones: 1,
+      twos: 0,
+      threes: 0,
+      fours: 0,
+      fives: 0,
+      sixes: 0,
+      threeOfAKind: 0,
+      fourOfAKind: 0,
+      fullHouse: 0,
+      smallStraight: 0,
+      largeStraight: 0,
+      yahtzee: 0,
+      chance: 0,
+      upperTotal: 1,
+      lowerTotal: 0,
+      grandTotal: 0,
+    }
+
+    const scoreSpy = jest.spyOn(Score, 'create').mockResolvedValue({
+      _id: '123456789',
+      ...score,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    } as any)
+
+    const yahtzeeRepository = new YahtzeeRepository()
+    const result = await yahtzeeRepository.createScore(score)
+
+    expect(scoreSpy).toHaveBeenCalledWith(score)
+    expect(result).toEqual({
+      _id: '123456789',
+      ...score,
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
+    })
+  })
+})
---
+++ src/yahtzee/yahtzee.repository.ts
@@ -0,0 +1,15 @@
+import Score, { IScoreInputData, IScoreScheema } from './model/score.model'
+
+class YahtzeeRepository {
+  async createScore(score: IScoreInputData): Promise<IScoreScheema> {
+    return await Score.create(score)
+  }
+
+  async getScoresByGame(game: string): Promise<IScoreScheema[]> {
+    return await Score.find({
+      game,
+    })
+  }
+}
+
+export default YahtzeeRepository
